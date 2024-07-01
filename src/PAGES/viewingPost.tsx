32ff31d2599b8fe6ba723176ae06a4649211996b
Darius Valere <darius.valere@reddit.com>
2024-07-01 09:04:02 -0400

    hm

--- src/PAGES/viewingPost.tsx
+++ src/PAGES/viewingPost.tsx
@@ -25,7 +25,7 @@ export let ViewingPost = ({ setPage, imageUrl, description }: PageProps & { imag
         <text weight="bold" alignment="top center" color="black" > What is this? </text>
         </hstack>
       </hstack>
-      <image url={global1} imageWidth={128} imageHeight={128} /> 
+      <image url={global2} imageWidth={128} imageHeight={128} /> 
       <text size="medium" color="black"> {global3} </text>
       <hstack alignment="top center" width="85%" height="15%" backgroundColor="PureGray-250">
           <button icon="comments" disabled={true} appearance="plain"></button>
